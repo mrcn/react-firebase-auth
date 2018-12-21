a9d7866c573551e8fa445d5e895e4b0b927c799e
Marcin Dabrowski <mdabrowski@mdrt.org>
2018-12-20 21:02:13 -0600

    ass firebase provider and consumer at top component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 // import 'semantic-ui-css/semantic.min.css'
+import Firebase, { FirebaseContext } from './components/Firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>,
+    
+    document.getElementById('root'));
 
 serviceWorker.unregister();

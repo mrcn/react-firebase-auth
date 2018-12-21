17b57512ea43fd92c825f0eb29aef61f9973cbf9
Marcin Dabrowski <mdabrowski@mdrt.org>
2018-12-20 20:44:50 -0600

    introducing firebase

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
 
-const Firebase = () => (
-    <div>
-        <h1>Firebase</h1>
-    </div>
-);
 
-export default Firebase;
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};

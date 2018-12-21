be6e7c2cbe35ef49133b85a7707abb460161b8e0
Marcin Dabrowski <mdabrowski@mdrt.org>
2018-12-20 21:40:41 -0600

    add semantic ui kit

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+// import '../../semantic-ui-react/dist/umd/semantic-ui-react.min';
+import '../../semantic-ui-css-master/semantic.min.css';
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+// import * as serviceWorker from './serviceWorker';
+// import Firebase, { FirebaseContext } from './components/Firebase';
+
+
 const SignUp = () => (
     <div>
         <h1>SignUp</h1>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-// import 'semantic-ui-css/semantic.min.css'
 import Firebase, { FirebaseContext } from './components/Firebase';
 
 
